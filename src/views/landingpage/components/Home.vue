79d306ea1bb53c86f669e28dfe3835760c45abe4
Otuedor Gideon <samsyotuedor40@gmail.com>
2022-03-25 08:53:59 +0100

    adjust the UI

--- src/views/landingpage/components/Home.vue
+++ src/views/landingpage/components/Home.vue
@@ -5,10 +5,24 @@
                 <div class="col-md-6">
                     <div class="text-white display-4 font-weight-bold  ">Fellow</div>
                 </div>
-                <div class="col-md-6 d-flex flex-wrap mt-3">
-                    <div class=""><button class="default-btn bg-primary text-white font-weight-bold">Sign in or Create account</button></div>
-                    <div class=" "><button class="default-btn bg-primary text-white font-weight-bold">Support us</button></div>
-                    <div class=""><button class="default-btn bg-primary text-white font-weight-bold"><i class="pi pi-search"></i> Search</button></div>
+                <div class="col-md-6 justify-content-between  d-flex flex-wrap mt-3">
+                    <div class=""><button class="btn bg-primary text-white font-weight-bold">Sign in or Create account</button></div>
+                    <div class=""><button class="btn bg-primary text-white font-weight-bold">About us</button></div>
+                    <div class=""><button class="btn bg-primary text-white font-weight-bold">Fellowship</button></div>
+                    <div class=""><button class="btn bg-primary text-white font-weight-bold"><i class="pi pi-search"></i> Search</button></div>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-md-12 d-flex justify-content-center">
+                    <div class=" col-md-5 display-1 text-white font-weight-bolder">Become a Fellow</div>
+                </div>
+                <div class="col-md-12 d-flex justify-content-center my-3">
+                    <div class="col-md-5 text-white font-weight-bold">Join a committed cohort of leaders intent who intend to build a gender-inclusive society, balanced and sustainable world</div>
+                </div>
+                <div class="col-md-12 d-flex justify-content-center">
+                    <div class="col-md-5 text-white ">
+                        <Dropdown v-model="selectFellow" class="w-100 bg-primary  font-weight-bold" :options="fellowList"  optionLabel="name" placeholder="Become a Fellow"/>
+                    </div>
                 </div>
             </div>
         </div>
@@ -16,9 +30,28 @@
 </template>
 
 <script>
+import { ref } from "vue";
+import Dropdown from "primevue/dropdown";
 export default {
+    components: {
+        Dropdown
+    },
+
     setup() {
-        
+
+        const selectFellow = ref('')
+        const fellowList = ref(
+            [
+                {name: "Become a fellow"},
+                {name: "Leadership"},
+                {name: "Fellowship"}
+            ]
+        )
+
+        return {
+            fellowList,
+            selectFellow
+        }
     },
 }
 </script>
